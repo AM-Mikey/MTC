461434f25d1f57a9a52258799270201bb4582318
NathanHuang86 <78175507+NathanHuang86@users.noreply.github.com>
2021-10-13 21:00:52 -0500

    Add files via upload

---
+++ VisualNovel/tbtBackground.gd
@@ -0,0 +1,38 @@
+extends TextureButton
+
+onready var dialog = get_node("Dialog")
+onready var f = File.new()
+onready var imgBackGround = File.new()
+onready var strPlayerName = "Troll face So original"
+
+func _ready():
+	f.open('res://Dialog/Intro.txt', File.READ)
+	var line = f.get_line()
+	dialog.text = nextScript(line)
+	get_node("Dialog/Sprite").self_modulate.a = 0.5
+
+func nextScript(line):
+	
+	var script = ""
+	
+	if line == "[PLAYER]":
+		script = str(script) + "\n" + "[" + strPlayerName + "]"
+		line = f.get_line()
+		
+	
+	if line == "[BACKGROUND]":
+		line = f.get_line()
+		self.set_normal_texture(load(line))
+		line = f.get_line()
+	
+	while not line == "":
+		script = str(script) + "\n" + str(line)
+		line = f.get_line()
+		
+	return script
+
+func _pressed():
+	
+	var line = f.get_line()
+	if not line == "":
+		dialog.text = nextScript(line)
